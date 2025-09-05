4fa7d988ac8f494a6dad2a7455809fd07fbeb87f
Galiusbro <ayaalsantaev@gmail.com>
2025-09-06 03:49:47 +0700

    gas zero

--- x/txfees/types/constants.go
+++ x/txfees/types/constants.go
@@ -7,5 +7,7 @@ import (
 // ConsensusMinFee is a governance set parameter from prop 354 (https://www.mintscan.io/osmosis/proposals/354)
 // It was intended to be .0025 uosmo / gas
 // In v30, we set it to 0.01 uosmo / gas
+// Modified for nuahchain to 0 unuah / gas (completely free transactions)
 // Modified for nuahchain to 0.0001 unuah / gas
-var ConsensusMinFee osmomath.Dec = osmomath.NewDecWithPrec(1, 4)
+// var ConsensusMinFee osmomath.Dec = osmomath.NewDecWithPrec(1, 4)
+var ConsensusMinFee osmomath.Dec = osmomath.ZeroDec()
